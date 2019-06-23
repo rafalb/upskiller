bef22fe1b161af7143ecc502c7d8d18bb9a1e8d9
rafalb <rafalbarszczewski85@gmail.com>
2019-06-23 14:54:40 +0200

    Enhanced test component (calculator)

--- upskiller/src/components/Calculator.js
+++ upskiller/src/components/Calculator.js
@@ -5,11 +5,13 @@ const Calculator = () => {
     const [number1, setNumber1] = useState(0);
     const [number2, setNumber2] = useState(0);
 
+    const recalculate = (n1, n2) => setSum(Number(n1) + Number(n2));
+
     return (
     <div>
-        <input value={number1} onInput={v => setNumber1(v.target.value)} />
-        <input value={number2} onInput={v => setNumber2(v.target.value)} />
-        <button onClick={() => setSum(Number(number1) + Number(number2))}>Sum</button>
+        <input value={number1} onInput={v => { setNumber1(v.target.value); recalculate(v.target.value, number2); }} />
+        <input value={number2} onInput={v => { setNumber2(v.target.value); recalculate(number1, v.target.value); } } />
+        <button onClick={() => recalculate(number1, number2)}>Sum</button>
         <p>Result: {sum}</p>
     </div>
     );
