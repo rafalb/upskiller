de6b82e9b043f06a843c08364ca092ce245ac0a4
rafalb <rafalbarszczewski85@gmail.com>
2019-06-23 16:02:18 +0200

    Areas and topics: Simple styles for the list, skill level display (random)

--- upskiller/src/App.js
+++ upskiller/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Calculator from './components/Calculator';
 import AreasSkillsTree from './components/AreasSkillsTree';
 import testData from './testData.json';
 
@@ -9,9 +8,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Calculator/>
         <AreasSkillsTree areas={testData["areas"]}/>
+        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
--- upskiller/src/components/AreasSkillsTree.js
+++ upskiller/src/components/AreasSkillsTree.js
@@ -1,13 +1,19 @@
 import React  from 'react';
+import './AreasSkillsTree.css';
 
 const AreasSkillsTree = (props) => {
     return (
-        <ul>
+        <ul className="areas">
             {props.areas.map(a =>
-            <li>
+            <li className="area">
                 {a.name}
-                <ul>
-                    {a.topics.map(t => <li>{t}</li>)}
+                <ul className="topics">
+                    {a.topics.map(t =>
+                    <li className="topic">
+                        <div className="topicTitle">{t}</div>
+                        <div className="topicSkill">{Array.from({length: Math.random() * 6}).map(x => "★")}</div>
+                    </li>
+                    )}
                 </ul>
             </li>)}
         </ul>
